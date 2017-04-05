58d19d0ef38a4403944ee35bf6ec49bfdc5f8def
Susant Pant <susantpant@hotmail.com>
2017-04-05 04:48:48 -0600

    got rid of things that didn't work

--- template.cpp
+++ template.cpp
@@ -597,8 +597,8 @@ GLFWwindow* createWindow()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
-	GLFWwindow* window = glfwCreateWindow(mode->width, mode->height-40,
+	glfwWindowHint(GLFW_DECORATED, GL_TRUE);
+	GLFWwindow* window = glfwCreateWindow(mode->width, mode->height,
 		"OpenGL Template", NULL, NULL);
 	if (!window)
 	{
@@ -737,29 +737,7 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
-
-    else if(key == GLFW_KEY_F11 && action == GLFW_PRESS)
-    {
-    	//Get the primary monitor and the monitor attached to the current window
-    	const GLFWvidmode* mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-    	GLFWmonitor* monitor = glfwGetWindowMonitor(window);
-
-    	//If no monitor is attached to the window attach the primary monitor
-    	//This makes the window full screen
-    	if(monitor==NULL)
-    		glfwSetWindowMonitor(window, glfwGetPrimaryMonitor(), 0, 0,
-    			mode->width, mode->height, mode->refreshRate);
-
-    	//If a monitor is attached, detach it and set the dimesnions to that of
-    	//The primary's monitor resolution. This makes the window windowed
-    	else
-    		glfwSetWindowMonitor(window, NULL, 0, 0,
-    			mode->width, mode->height-1, mode->refreshRate);
-    			//subtract 1 to prevent it from going into full screen mode
-
-    	glfwMaximizeWindow(window);
-    }
+        glfwSetWindowShouldClose(window, GL_TRUE);
 
     else if(key == GLFW_KEY_F12 && action == GLFW_PRESS)
     	cout << glfwGetVersionString() << endl;
